2e7f829110daf3123355a37c1ff7cb2d8c306971
maneekwant <32344171+maneekwant@users.noreply.github.com>
2020-11-19 15:26:02 -0800

    Update generate_data.sh

--- CODES_data_generation/generate_data.sh
+++ CODES_data_generation/generate_data.sh
@@ -1,5 +1,5 @@
-python get_classified_lidar_pc.py
+python get_classified_lidar_pc.py 
 python grid_generation.py
 python evidential_grid_generation.py
 python get_evidential_splits_full_dynmask.py
-python subsample_evidential_grid_splits_training.py
+python subsample_evidential_grid_splits_training.py # Subsample for only grids with moving objects for the training data
