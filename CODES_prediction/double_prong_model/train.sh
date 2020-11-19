d447e8dbe7f8d7c97b17630a49e3533a1ce75a09
maneekwant <32344171+maneekwant@users.noreply.github.com>
2020-11-19 15:32:52 -0800

    Update train.sh

--- CODES_prediction/double_prong_model/train.sh
+++ CODES_prediction/double_prong_model/train.sh
@@ -1,4 +1,5 @@
 python train_t_1.py -o trained_t_1_model -ps prednet -pd prednet
 python train_t_5.py -o trained_t_5_model -f trained_t_1_model -ps prednet -pd prednet # fine-tuning
 
-# Please add the '-g' argument if wish to train on more than the default number of gpu of 1. 
+# Please add the '-g' argument if wish to train on more than the default number of gpu of 1. e.g.
+# python train_t_1.py -o trained_t_1_model -ps prednet -pd prednet -g 3 -> train on 3 gpus
